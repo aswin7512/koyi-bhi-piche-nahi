6f95b4b510688b6bd8c6e709fff7a0491b22b0af
Aswin <aswinnrd09@gmail.com>
2026-01-03 22:11:18 +0530

    Some configs for github pages

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uwfcpffigojngpwvkynh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3ZmNwZmZpZ29qbmdwd3ZreW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0Mzc1NDUsImV4cCI6MjA4MzAxMzU0NX0.E_hnGdgdQbAhw3Z3BdgTHLdaCvUxMU9e3RDZ1LYMplY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("🚨 Supabase keys are missing! Check your .env file or Vercel/Netlify settings.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
